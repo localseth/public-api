2dc235ac8e9887ea36efb4dcc1206f74ca0247a3
Seth M <97685638+localseth@users.noreply.github.com>
2022-05-06 19:08:30 -0500

    add modal content to app.js, improve css layout

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const urlAPI = 'https://randomuser.me/api/?results=12&inc=name,picture,email,loc
 const gridContainer = document.querySelector('.grid-container');
 const overlay = document.querySelector('.overlay');
 const modalContainer = document.querySelector('.modal-content');
-const modalClose = document.querySelector('modal-close');
+const modalClose = document.querySelector('.modal-close');
 // -------------------------
 // data calls
 // -------------------------
@@ -42,15 +42,62 @@ function displayEmployees (employeeData) {
 
         const employeeHTML = `
             <div class="card" data-index="${index}">
-            <img class="avatar" src="${picture.large}" />
-            <div class="text-container">
-                <h2 class="name">${name.first} ${name.last}</h2>
-                <p class="email">${email}</p>
-                <p class="address">${city}, ${state}</p>
-            </div>
+                <img class="avatar" src="${picture.large}" />
+                <div class="text-container">
+                    <h2 class="name">${name.first} ${name.last}</h2>
+                    <p class="email">${email}</p>
+                    <p class="address">${city}, ${state}</p>
+                </div>
             </div>
         `;
 
         gridContainer.insertAdjacentHTML('beforeend', employeeHTML)
     });
 }
+
+function displayModal (index) {
+    //deconstruct array to more simply write the HTML template
+    let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employees[index];
+
+    let date = new Date(dob.date);
+    //template for overlay information
+    const modalHTML =`
+        <img class="avatar" src="${picture.large}" />
+        <div class="text-container">
+            <h2 class="name">${name.first} ${name.last}</h2>
+            <p class="email">${email}</p>
+            <p class="address">${city}</p>
+            <hr />
+            <p>${fixPhoneNum(phone)}</p>
+            <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
+            <p>Birthday: ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        </div>
+    `;
+
+    overlay.classList.remove('hidden');
+    modalContainer.insertAdjacentHTML('beforeend', modalHTML);
+}
+
+function fixPhoneNum (num) {
+    if (num.charAt(5) === '-') {
+        num = num.replace(num.charAt(5), ' ');
+    }
+    return num;
+}
+
+// -------------------------
+// event listeners
+// -------------------------
+gridContainer.addEventListener('click', e => {
+    if (e.target !== gridContainer) {
+        const card = e.target.closest('.card');
+        const index = card.getAttribute('data-index');
+
+        displayModal(index);
+    }
+})
+
+modalClose.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+    modalContainer.innerHTML = '';
+})
