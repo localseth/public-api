cf6739f830e7994370699c447f3a25b2260f97d3
Seth M <97685638+localseth@users.noreply.github.com>
2022-05-09 19:00:35 -0500

    add navigation buttons to overlay, add navigation feature to app.js

--- app.js
+++ app.js
@@ -2,12 +2,17 @@
 // global variables
 // -------------------------
 let employees = [];
+let filterIndexArray = employees;
 const urlAPI = 'https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=US';
 const gridContainer = document.querySelector('.grid-container');
+const allCards = document.querySelectorAll('.card');
 const overlay = document.querySelector('.overlay');
 const modalContainer = document.querySelector('.modal-content');
 const modalClose = document.querySelector('.modal-close');
 const search = document.getElementById('search');
+const searchBtn = document.getElementById('search-button');
+const leftArrow = document.querySelector('.fa-circle-arrow-left');
+const rightArrow = document.querySelector('.fa-circle-arrow-right');
 // -------------------------
 // data calls
 // -------------------------
@@ -62,6 +67,7 @@ function displayModal (index) {
     let { name, dob, phone, email, location: { city, street, state, postcode}, picture } = employees[index];
 
     let date = new Date(dob.date);
+    modalContainer.innerHTML = '';
     //template for overlay information
     const modalHTML =`
         <img class="avatar" src="${picture.large}" />
@@ -87,27 +93,55 @@ function fixPhoneNum (num) {
     return num;
 }
 
+function getIndex(email) {
+    let employeeIndex = 0;
+    employees.forEach( (employee, index) => {
+        if (employee.email === email) {
+            employeeIndex = index;
+        }
+    });
+    return employeeIndex;
+}
+
+// function filterAndDisplay () {
+//     const filterArray = employees.filter(employee => 
+//         `${employee.name.first.toLowerCase()} ${employee.name.last.toLowerCase()}`.includes(search.value.toLowerCase())
+//     )
+//     console.log(filterArray);
+//     if (!/^\s*$/g.test(search.value)) {
+//         gridContainer.innerHTML = '';
+//         if (filterArray.length > 0){
+//             displayEmployees(filterArray);
+//         }
+//         if (filterArray.length === 0) {
+//             gridContainer.innerHTML = `<h1>Your search for "${search.value}" returned no results. Please try again.</h1>`;
+//         }
+//     } else {
+//         gridContainer.innerHTML = '';
+//         displayEmployees(employees)
+//     }
+// }
+
 function filterAndDisplay () {
-    const filterArray = employees.filter(employee => 
-        `${employee.name.first.toLowerCase()} ${employee.name.last.toLowerCase()}`.includes(search.value.toLowerCase())
+    filterIndexArray = [];
+    employees.forEach(employee => {
+            if ( `${employee.name.first.toLowerCase()} ${employee.name.last.toLowerCase()}`
+                .includes(search.value
+                    .toLowerCase()) || search.value === '' ) {
+                document.querySelector(`div[data-index="${getIndex(employee.email)}"`).style.display = "flex";
+                filterIndexArray.push(getIndex(employee.email));
+            } else {
+                document.querySelector(`div[data-index="${getIndex(employee.email)}"`).style.display = "none";
+            }
+        }
     )
-    if (!/^\s*$/g.test(search.value)) {
-        gridContainer.innerHTML = '';
-        if (filterArray.length > 0){
-            displayEmployees(filterArray);
-        }
-        if (filterArray.length === 0) {
-            gridContainer.innerHTML = `<h1>Your search for "${search.value}" returned no results. Please try again.</h1>`;
-        }
-    } else {
-        gridContainer.innerHTML = '';
-        displayEmployees(employees)
-    }
 }
 
 // -------------------------
 // event listeners
 // -------------------------
+
+//display details of student
 gridContainer.addEventListener('click', e => {
     if (e.target !== gridContainer && e.target !== overlay) {
         if (e.target.closest('.card') !== null) {
@@ -118,9 +152,30 @@ gridContainer.addEventListener('click', e => {
     }
 })
 
+//close detail window
 modalClose.addEventListener('click', () => {
     overlay.classList.add('hidden');
     modalContainer.innerHTML = '';
 })
 
+//search each time input changes value
 search.addEventListener('input', filterAndDisplay)
+searchBtn.addEventListener('click', filterAndDisplay);
+
+
+//navigation buttons
+leftArrow.addEventListener('click', () => {
+    const email = document.querySelector('.modal .email').innerText;
+    if (getIndex(email) > filterIndexArray[0]) {
+        const filterIndex = filterIndexArray.indexOf(getIndex(email));
+        displayModal(filterIndexArray[filterIndex - 1]);
+    } else displayModal(filterIndexArray[filterIndexArray.length - 1]);
+})
+
+rightArrow.addEventListener('click', () => {
+    const email = document.querySelector('.modal .email').innerText;
+    if (getIndex(email) < filterIndexArray[filterIndexArray.length - 1]) {
+        const filterIndex = filterIndexArray.indexOf(getIndex(email));
+        displayModal(filterIndexArray[filterIndex + 1]);
+    } else displayModal(filterIndexArray[0]);
+})
