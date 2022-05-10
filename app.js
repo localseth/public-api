70698ecbf045e843104b4d35cdcfc84d310ff398
Seth M <97685638+localseth@users.noreply.github.com>
2022-05-09 20:29:34 -0500

    fix search functionality to not lose inital populated data

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // global variables
 // -------------------------
 let employees = [];
-let filterIndexArray = employees;
+let filterIndexArray = [];
 const urlAPI = 'https://randomuser.me/api/?results=12&inc=name,picture,email,location,phone,dob&noinfo&nat=US';
 const gridContainer = document.querySelector('.grid-container');
 const allCards = document.querySelectorAll('.card');
@@ -13,13 +13,15 @@ const search = document.getElementById('search');
 const searchBtn = document.getElementById('search-button');
 const leftArrow = document.querySelector('.fa-circle-arrow-left');
 const rightArrow = document.querySelector('.fa-circle-arrow-right');
+const noResults = document.querySelector('.no-results');
 // -------------------------
 // data calls
 // -------------------------
 fetchData(urlAPI)
     .then((res) => {
-        employees = res.results
-        displayEmployees(employees)
+        employees = res.results;
+        displayEmployees(employees);
+        filterAndDisplay();
     })
 // -------------------------
 // helper functions
@@ -93,6 +95,7 @@ function fixPhoneNum (num) {
     return num;
 }
 
+//finds index of employees[] based on email address
 function getIndex(email) {
     let employeeIndex = 0;
     employees.forEach( (employee, index) => {
@@ -103,26 +106,9 @@ function getIndex(email) {
     return employeeIndex;
 }
 
-// function filterAndDisplay () {
-//     const filterArray = employees.filter(employee => 
-//         `${employee.name.first.toLowerCase()} ${employee.name.last.toLowerCase()}`.includes(search.value.toLowerCase())
-//     )
-//     console.log(filterArray);
-//     if (!/^\s*$/g.test(search.value)) {
-//         gridContainer.innerHTML = '';
-//         if (filterArray.length > 0){
-//             displayEmployees(filterArray);
-//         }
-//         if (filterArray.length === 0) {
-//             gridContainer.innerHTML = `<h1>Your search for "${search.value}" returned no results. Please try again.</h1>`;
-//         }
-//     } else {
-//         gridContainer.innerHTML = '';
-//         displayEmployees(employees)
-//     }
-// }
-
+//displays only matches to search results
 function filterAndDisplay () {
+    noResults.style.display = 'none'
     filterIndexArray = [];
     employees.forEach(employee => {
             if ( `${employee.name.first.toLowerCase()} ${employee.name.last.toLowerCase()}`
@@ -135,11 +121,22 @@ function filterAndDisplay () {
             }
         }
     )
+    if (filterIndexArray.length === 0) {
+        allCards.forEach(card => card.style.display = 'none');
+        noResults.innerText = `Your search for "${search.value}" returned no results. Please try again`;
+        noResults.style.display = 'flex';
+    }
 }
+
 
 // -------------------------
 // event listeners
 // -------------------------
+
+//default load settings
+window.addEventListener('load', () => {
+    search.value = '';
+})
 
 //display details of student
 gridContainer.addEventListener('click', e => {
@@ -161,7 +158,6 @@ modalClose.addEventListener('click', () => {
 //search each time input changes value
 search.addEventListener('input', filterAndDisplay)
 searchBtn.addEventListener('click', filterAndDisplay);
-
 
 //navigation buttons
 leftArrow.addEventListener('click', () => {
